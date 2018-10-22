b48121cd4fb25dcf1291ae89a09380debd5b74b0
hongliang5 <hongliang5@jd.com>
2018-10-22 18:24:51 +0800

    添加摄像头实现。

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -16,6 +16,7 @@ import {Camera, CameraOptions} from "@ionic-native/camera";
 export class CameraPage {
   strUrl:string;
   medio:HTMLElement;
+  base64Image;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
     this.strUrl = 'http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8';
@@ -33,16 +34,15 @@ export class CameraPage {
     console.log("打开摄像头");
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     };
 
     this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64 (DATA_URL):
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-      console.log("获取的图片："+base64Image);
+      // imageData is either a base64 encoded string or a file URI If it's base64 (DATA_URL):
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      // console.log("获取的图片："+this.base64Image);
     }, (err) => {
       // Handle error
     });
