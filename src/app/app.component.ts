fec610ea123fa10a462a2c5c5b95f3c467877926
hllinc <hllinc@163.com>
2018-10-22 23:16:20 +0800

    修复摄像头打不开问题。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {RegisterPage} from '../pages/register/register';
 import {SysPage} from '../pages/sys/sys';
 import {WelcomePage} from '../pages/welcome/welcome';
 import {Storage} from '@ionic/storage';
+import {Camera, CameraOptions} from '@ionic-native/camera';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,8 +17,9 @@ import {Storage} from '@ionic/storage';
 export class MyApp {
   @ViewChild('content') nav;
   rootPage: any = TabsPage;
+  base64Image;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public modalCtrl: ModalController, public storage: Storage, public menu: MenuController) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public modalCtrl: ModalController, public storage: Storage, public menu: MenuController, private camera: Camera) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -49,5 +51,25 @@ export class MyApp {
   openSysPage(){
     this.nav.push(SysPage);
     this.menu.toggle();
+  }
+
+  openCamera(){
+    console.log("打开摄像头");
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      // destinationType: this.camera.DestinationType.FILE_URI,// 这里要根据不同平台切换以提高效率，ios为FILE_URI，android为NATIVE_URI，否则app可能会因内存溢出而闪退
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI If it's base64 (DATA_URL):
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      // this.base64Image = imageData;
+      // console.log("获取的图片："+this.base64Image);
+    }, (err) => {
+      // Handle error
+    });
   }
 }
--- src/pages/liveBroadcast/liveBroadcast.ts
+++ src/pages/liveBroadcast/liveBroadcast.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {Camera, CameraOptions} from '@ionic-native/camera';
 
 /**
  * Generated class for the LiveBroadcastPage page.
@@ -16,9 +15,8 @@ import {Camera, CameraOptions} from '@ionic-native/camera';
 export class LiveBroadcastPage {
   strUrl:string;
   medio:HTMLElement;
-  base64Image;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.strUrl = 'http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8';
   }
 
@@ -28,26 +26,6 @@ export class LiveBroadcastPage {
   ionViewDidEnter(){
     this.medio = document.getElementById("vidioID");
     //在这里就可以添加相应的代码对medio进行控制了
-  }
-
-  openCamera(){
-    console.log("打开摄像头");
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      // destinationType: this.camera.DestinationType.FILE_URI,// 这里要根据不同平台切换以提高效率，ios为FILE_URI，android为NATIVE_URI，否则app可能会因内存溢出而闪退
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    };
-
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI If it's base64 (DATA_URL):
-      this.base64Image = 'data:image/jpeg;base64,' + imageData;
-      // this.base64Image = imageData;
-      // console.log("获取的图片："+this.base64Image);
-    }, (err) => {
-      // Handle error
-    });
   }
 
 }
